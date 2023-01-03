36f4915355d57f1efb3f896741d606e6f9e71e6a
Давлат Ушурбакиев <davlatbek@tredo.co>
2023-01-03 17:04:46 +0600

    refactored styles for views of status codes

--- views/403.php
+++ views/403.php
@@ -4,7 +4,12 @@
 
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <p>Sorry,you have no access to this page</p>
+            <h1 class="text-2xl font-bold">Sorry,you have no access to this page</h1>
+
+            <p class="mt-4">
+                <a href="/" class="text-blue-500 underline">Back to main page</a>
+            </p>
+
         </div>
     </main>
 
--- views/404.php
+++ views/404.php
@@ -4,7 +4,12 @@
 
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <p>Sorry, page not found</p>
+            <h1 class="text-2xl font-bold">Sorry, page not found</h1>
+
+            <p class="mt-4">
+                <a href="/" class="text-blue-500 underline">Back to main page</a>
+            </p>
+
         </div>
     </main>
 
