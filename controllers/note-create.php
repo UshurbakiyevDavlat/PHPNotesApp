1963abea78832ae7ef32790d4f5d607c053b41f3
Давлат Ушурбакиев <dushurbakiev@gmail.com>
2023-05-14 10:22:06 +0600

    added old value for form instead of reseting, validation cases, result and error cases , try catching

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -4,14 +4,36 @@ $config = Config::env();
 $statement = 'INSERT INTO notes (body, user_id) VALUES (:body, :user_id)';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $query_params = [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ];
+    $errors = [];
+    $data = $_POST;
+    $body = trim($data['body']);
 
-    $db = new Database($config['database'], $statement);
+    if (empty($body)) {
+        $errors [] = 'The note body is required.';
+    }
 
-    $db->query($query_params)->find();
+    if (strlen($body) < 10) {
+        $errors[] = 'The note body must be at least 10 characters. You have entered ' . strlen($body) . ' characters.';
+    }
+
+    if (strlen($body) > 1000) {
+        $errors[] = 'The note body must be less than 1000 characters.';
+    }
+
+    if (empty($errors)) {
+        $query_params = [
+            'body' => $body,
+            'user_id' => 1
+        ];
+
+        $db = new Database($config['database'], $statement);
+        try {
+            $db->query($query_params);
+            $result = 'Note created successfully.';
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
 }
 
 require 'views/note-create.view.php';
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -12,7 +12,16 @@
                         <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
                         <div class="mt-2">
                             <input type="text" name="body" id="body" autocomplete="given-body"
+                                   value="<?= $body ?? '' ?>"
                                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            <?php
+                            if (!empty($errors)) {
+                                echo '<p class="text-red-500 text-sm mt-1">' . (implode('', $errors)) . '</p>';
+                            } elseif (!empty($result)) {
+                                echo '<p class="text-green-500 text-sm mt-1">' . $result . '</p>';
+                            }
+                            ?>
+
                         </div>
                     </div>
                 </div>
