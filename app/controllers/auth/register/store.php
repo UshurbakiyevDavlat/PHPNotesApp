66f7514fb85d80cbf2fa88eef694fcf5e08fa8ff
Ушурбакиев Давлатбек Алимжанович <davlatbek.ushurbakiyev@ffin.kz>
2024-04-29 14:19:23 +0600

    added password hashing in users registration

--- app/controllers/auth/register/store.php
+++ app/controllers/auth/register/store.php
@@ -46,7 +46,7 @@ if ($user) {
     $db->query('INSERT INTO users (email,password) VALUES (:email,:password)',
         [
             'email' => $email,
-            'password' => $password,
+            'password' => password_hash($password, PASSWORD_DEFAULT), //for now default is BCRYPT
         ],
     );
 
